bf7620147755fe71e3b229af051c595b14ae41ed
Aufal <marromaufal17@gmail.com>
2025-09-21 19:06:29 +0700

    Delete DICUACA directory

--- DICUACA/login.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("btn-login").addEventListener("click", function() {
-  window.location.href = "map.html"; 
-
-})
--- DICUACA/welcome.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("btnStart").addEventListener("click", function() {
-  window.location.href = "login.html"; 
-
-setTimeout(() => {
-    window.location.href = "login.html";
-  }, 800); 
-});
